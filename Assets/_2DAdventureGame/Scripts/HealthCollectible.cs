dba38ce39638a5d340a8d155760e301aac397fbc
trong-nghia84 <nghiakha08@gmail.com>
2026-04-02 16:30:53 +0700

    add Health collect + second update

---
+++ Assets/_2DAdventureGame/Scripts/HealthCollectible.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class HealthCollectible : MonoBehaviour
+{
+    
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            PlayerController controller = other.GetComponent<PlayerController>();
+
+            if (controller != null)
+            {
+                controller.ChangeHealth(1);
+                Destroy(gameObject);
+            }
+        }
+ 
+}
--- Assets/_2DAdventureGame/Scripts/PlayerController.cs
+++ Assets/_2DAdventureGame/Scripts/PlayerController.cs
@@ -7,26 +7,34 @@ public class PlayerController : MonoBehaviour
     public InputAction MoveAction;
     Rigidbody2D rigidbody2d;
     Vector2 move;
+    public int maxHealth = 5;
+    int currentHealth = 1;
+    public float speed = 3.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //QualitySettings.vSyncCount = 0;
         rigidbody2d = GetComponent<Rigidbody2D>();
-        //Application.targetFrameRate = 10;
         MoveAction.Enable();
+        //currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
         move = MoveAction.ReadValue<Vector2>();
-
-        Debug.Log(move);
+        //Debug.Log(move);
     }
 
     private void FixedUpdate()
     {
-        Vector2 position = (Vector2)rigidbody2d.position + move * 3.0f * Time.deltaTime;
+        Vector2 position = (Vector2)rigidbody2d.position + move * speed * Time.deltaTime;
         rigidbody2d.MovePosition(position);
+    }
+
+     public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+
     }
 }
