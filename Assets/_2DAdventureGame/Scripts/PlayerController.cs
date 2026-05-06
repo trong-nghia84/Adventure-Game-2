3d0d636aff3a41e3a9af1092f2906045c760df8e
trong-nghia84 <nghiakha08@gmail.com>
2026-05-06 23:19:33 +0700

    AddAnimationPlayer + Add Enemy

--- Assets/_2DAdventureGame/Scripts/PlayerController.cs
+++ Assets/_2DAdventureGame/Scripts/PlayerController.cs
@@ -1,57 +1,103 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-
 public class PlayerController : MonoBehaviour
 {
+    // Variables related to player character movement
     public InputAction MoveAction;
     Rigidbody2D rigidbody2d;
     Vector2 move;
+    public float speed = 3.0f;
+
+    // Variables related to the health system
     public int maxHealth = 5;
     int currentHealth;
     public int health { get { return currentHealth; } }
-    public float speed = 3.0f;
 
+    // Variables related to temporary invincibility
     public float timeInvincible = 2.0f;
     bool isInvincible;
     float damageCooldown;
+
+    // Variables related to animation
+    Animator animator;
+    Vector2 moveDirection = new Vector2(1, 0);
+
+    // Variables related to projectiles
+    public GameObject projectilePrefab;
+    public InputAction LaunchAction;
+
+    // Start is called before the first frame update
     void Start()
     {
-        rigidbody2d = GetComponent<Rigidbody2D>();
         MoveAction.Enable();
+        LaunchAction.Enable();
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
         currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
+        move = MoveAction.ReadValue<Vector2>();
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            moveDirection.Set(move.x, move.y);
+            moveDirection.Normalize();
+        }
+
+        animator.SetFloat("Look X", moveDirection.x);
+        animator.SetFloat("Look Y", moveDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
         if (isInvincible)
         {
             damageCooldown -= Time.deltaTime;
             if (damageCooldown < 0)
+            {
                 isInvincible = false;
+            }
         }
-        move = MoveAction.ReadValue<Vector2>();
-        //Debug.Log(move);
+
+        if (LaunchAction.WasPressedThisFrame())
+        {
+            Launch();
+        }
+
     }
 
-    private void FixedUpdate()
+    // FixedUpdate has the same call rate as the physics system
+    void FixedUpdate()
     {
         Vector2 position = (Vector2)rigidbody2d.position + move * speed * Time.deltaTime;
         rigidbody2d.MovePosition(position);
     }
 
-     public void ChangeHealth(int amount)
+    public void ChangeHealth(int amount)
     {
         if (amount < 0)
         {
             if (isInvincible)
+            {
                 return;
+            }
+
             isInvincible = true;
             damageCooldown = timeInvincible;
+            animator.SetTrigger("Hit");
         }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
 
+    void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(moveDirection, 300);
+        animator.SetTrigger("Launch");
     }
 }
---
+++ Assets/_2DAdventureGame/Scripts/Projectile.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    Rigidbody2D rigidbody2d;
+
+    // Awake is called when the Projectile GameObject is instantiated
+    void Awake()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (transform.position.magnitude > 100.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Launch(Vector2 direction, float force)
+    {
+        rigidbody2d.AddForce(direction * force);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        EnemyController enemy = other.GetComponent<EnemyController>();
+        if (enemy != null)
+        {
+            enemy.Fix();
+        }
+
+        Destroy(gameObject);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
